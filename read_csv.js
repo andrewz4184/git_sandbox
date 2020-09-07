091600e5f1f69df812cc32bd1985648592d46ad0
Foogle7 <69803093+Foogle7@users.noreply.github.com>
2020-09-07 15:48:22 -0400

    Add files via upload
    Dropdown menu showing the items on the first line of the csv file

---
+++ read_csv.js
@@ -0,0 +1,62 @@
+/*
+$(document).ready(function() {
+	$("#upload").on("click",function(){
+        console.log("hello");
+        var fr = new FileReader();
+        fr.onload = loadHandler;
+        fr.onerror = errorHandler;
+        fr.readAsText(this.files[0]);
+
+    })
+});
+
+function parseCSV(csv_content) {
+    d3_parser = d3.csvParse(csv_content)
+    console.log(d3_parser)
+}
+
+function loadHandler(event) {
+    var csv = event.target.result;
+    parseCSV(csv);
+}
+
+function errorHandler(evt) {
+    if (evt.target.error.name == "NotReadableError") {
+        alert("Canno't read file !");
+    }
+}
+*/
+var dropdownClickShow = false;
+
+var names = [];
+var i = 0;
+$(document).ready(function () {
+
+    $("#dropdown").on("click", function () {
+        if (dropdownClickShow) {
+            $("ul.list").hide();
+        } else {
+            $("ul.list").show();
+        }
+        dropdownClickShow = !dropdownClickShow;
+        d3.csv("TechCrunchcontinentalUSA.csv").then(function (data) {
+         
+            Object.keys(data[0]).forEach((prop) => names.push(prop)); //Grabs the object properties and stores it into an array
+            
+            
+            while(i < names.length){ 
+                $("ul.list").append("<li>" + names[i] + "</li>"); 
+                i++;
+            }
+            i = 500;
+           
+            
+        });
+
+        
+    });
+   
+    
+    
+});
+
